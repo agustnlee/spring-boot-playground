ca853ead59456b43f0617712e577077500a5c252
agustnlee <lyoonsoo25@gmail.com>
2026-03-04 02:45:34 -0300

    Initial SpringBoot setup

---
+++ backend/src/test/java/com/agustin/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
